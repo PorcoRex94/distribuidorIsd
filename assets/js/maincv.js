fece7e9307493ec667cbc84bf7027300973ae4bc
Guido Moras <guidomoras94@gmail.com>
2024-10-25 16:49:10 -0300

    Cambios

--- assets/js/maincv.js
+++ assets/js/maincv.js
@@ -1148,6 +1148,55 @@ const createSection = (section) => {
   return sectionElement;
 };
 
+const nav = document.querySelector(".navbar");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const enlacesMenu = document.querySelectorAll(".navbar-list a");
+const rubrosLink = document.getElementById("toggleRubros");
+const dropdownContent = document.querySelector(".dropdown-content");
+const header = document.querySelector("#header");
+const body = document.body;
+
+// Función para abrir el menú
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+  document.body.classList.add("no-scroll"); // Bloqueamos el scroll cuando se abre el menú
+  header.style.position = "static";
+});
+
+// Función para cerrar el menú
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+  document.body.classList.remove("no-scroll"); // Habilitamos el scroll cuando se cierra el menú
+  header.style.position = "fixed";
+});
+
+// Función para ocultar el menú al hacer clic en enlaces que no son "Rubros"
+enlacesMenu.forEach((enlace) => {
+  enlace.addEventListener("click", (e) => {
+    // Si el enlace clickeado no es el de "Rubros", cerramos el menú
+    if (!enlace.closest(".dropdown")) {
+      nav.classList.remove("visible");
+      document.body.classList.remove("no-scroll"); // Habilitamos el scroll
+      header.style.position = "fixed";
+    }
+  });
+});
+
+document.addEventListener("click", (e) => {
+  // Verifica si el clic fue fuera del menú y del botón de abrir
+  if (!nav.contains(e.target) && !abrir.contains(e.target)) {
+    nav.classList.remove("visible");
+    document.body.classList.remove("no-scroll");
+    header.style.position = "fixed";
+  }
+});
+
+rubrosLink.addEventListener("click", (e) => {
+  e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+  dropdownContent.classList.toggle("active"); // Mostrar/ocultar el submenú de Rubros
+});
+
 // Renderizar todas las secciones con sus impresoras
 const container = document.getElementById("printer-container");
 sections.forEach((section) => container.appendChild(createSection(section)));
